69aba81fa35b6b6b572d316f8ab512a0cc52054c
grayziace <166556100+grayziace@users.noreply.github.com>
2026-07-14 16:35:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
     status.style.backgroundColor = "var(--neon-cyan)";
     status.style.boxShadow = "0 0 10px var(--neon-cyan)";
 });
+
+window.addEventListener('scroll', () => {
+    const scroll = window.scrollY;
+    const skyline = document.getElementById('skyline');
+    // Moves the background slower than the scroll
+    skyline.style.transform = `translateY(${scroll * 0.3}px)`;
+});
