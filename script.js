b477d0076e825a036f4ceaa1d32435b65e5f340b
grayziace <166556100+grayziace@users.noreply.github.com>
2026-07-14 16:26:56 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Initializing Project Shenzhen...");
+    const status = document.getElementById("status-light");
+    status.style.backgroundColor = "var(--neon-cyan)";
+    status.style.boxShadow = "0 0 10px var(--neon-cyan)";
+});
